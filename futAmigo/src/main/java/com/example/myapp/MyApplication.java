4454149f3217606574d443766668589bca5a01a2
PedrovlBarbosa <pvlb@cin.ufpe.br>
2023-12-12 17:31:56 -0300

    try to start using api google

---
+++ futAmigo/src/main/java/com/example/myapp/MyApplication.java
@@ -0,0 +1,5 @@
+package com.example.myapp;
+
+public class MyApplication {
+    
+}
---
+++ futAmigo/src/main/java/com/example/myapp/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.myapp.config;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer.JwtConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .requestMatchers("/login**").permitAll()
+                .anyRequest().authenticated())
+            .oauth2Login(withDefaults());
+        return http.build();
+    }
+}
+
+
