c41708a27c6e18bfc732ce1d2e52a92185e40a33
PedrovlBarbosa <pvlb@cin.ufpe.br>
2023-12-14 19:23:49 -0300

    create

---
+++ aps/src/test/java/com/example/aps/FutebolAmigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutebolAmigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
