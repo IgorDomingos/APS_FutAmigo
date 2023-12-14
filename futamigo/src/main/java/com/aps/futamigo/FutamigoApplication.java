edf340e793e4fc2de40277756a8303df0cbee433
PedrovlBarbosa <pvlb@cin.ufpe.br>
2023-12-14 19:18:04 -0300

    Create Project

---
+++ futamigo/src/main/java/com/aps/futamigo/FutamigoApplication.java
@@ -0,0 +1,13 @@
+package com.aps.futamigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutamigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutamigoApplication.class, args);
+	}
+
+}
