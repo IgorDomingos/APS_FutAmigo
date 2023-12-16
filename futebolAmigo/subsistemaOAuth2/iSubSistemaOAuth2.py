ef2b10cb3fcca18fc8d468dffd7006d33c326433
PedrovlBarbosa <pvlb@cin.ufpe.br>
2023-12-16 02:07:26 -0300

    update

--- futebolAmigo/subsistemaOAuth2/iSubSistemaOAuth2.py
+++ futebolAmigo/subsistemaOAuth2/iSubSistemaOAuth2.py
@@ -1,11 +1,14 @@
+from requests import HTTPError
 from oauth2client.client import OAuth2WebServerFlow
 from utils import SingletonMetaclass
 from negocio.controladores.controladorConta import ControladorConta
 from negocio.cadastro.cadastroConta import CadastroConta
+from oauth2client.client import InsufficientScopeError
+from oauth2client.client import TokenExpiredError
 
 class iSubsistemaOAuth2(metaclass=SingletonMetaclass):
     def __init__(self, config):
-        # Configure o fluxo OAuth2
+        # Configure OAuth2 flow
         self.flow = OAuth2WebServerFlow(
             client_id=config['client_id'],
             client_secret=config['client_secret'],
@@ -16,15 +19,22 @@ class iSubsistemaOAuth2(metaclass=SingletonMetaclass):
         try:
             # Verifica a validade do token
             credentials = self.flow.step2_exchange(token)
-            # Obtém informações do usuário
+            # Obtém informações do usuário com base na biblioteca escolhida
             user_info = credentials.getuserinfo()
-            return user_info
-        except Exception as e:
-            # Trata o erro
-            print(e)
-            return None
 
-# ...
+            # Seleciona informações relevantes
+            return {
+                'id': user_info['sub'],
+                'name': user_info['name'],
+                'email': user_info['email'],
+            }
+        except TokenExpiredError:
+            raise HTTPError(401, 'Token expirado')
+        except InsufficientScopeError:
+            raise HTTPError(403, 'Token com escopo insuficiente')
+        except Exception as e:
+            print(e)
+            raise HTTPError(401, 'Token inválido')
 
 controlador_conta = ControladorConta(CadastroConta, iSubsistemaOAuth2(config))
 
