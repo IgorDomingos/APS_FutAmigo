85dbbeb49517eb82245fb79905d9d540ee9b5ff6
PedrovlBarbosa <pvlb@cin.ufpe.br>
2024-02-19 02:51:16 -0300

    implements gestor

--- Spring/FutebolAmigo/src/main/java/br/ufpe/cin/futebolamigo/controllers/GestorController.java
+++ Spring/FutebolAmigo/src/main/java/br/ufpe/cin/futebolamigo/controllers/GestorController.java
@@ -1,55 +1,32 @@
 package br.ufpe.cin.futebolamigo.controllers;
 
-import br.ufpe.cin.futebolamigo.models.Gestor;
+import br.ufpe.cin.futebolamigo.dto.GestorDTO;
 import br.ufpe.cin.futebolamigo.services.GestorService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/gestor")
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class GestorController {
+    private final GestorService gestorService;
 
-//    public GestorController(GestorService gestorService) {
-//        this.gestorService = gestorService;
-//    }
-//    private final GestorService gestorService;
-//
-//    @PostMapping
-//    public ResponseEntity create(@RequestBody Gestor gestor, UriComponentsBuilder bodybuilder){
-//        var aux = gestorService.createGestor(gestor);
-//        var uri = bodybuilder.path("/gestor/{id}").buildAndExpand(aux.getCpf()).toUri();
-//        return ResponseEntity.created(uri).body(aux);
-//    }
-//    @GetMapping
-//    public ResponseEntity<Page<Gestor>> getAll(@PageableDefault Pageable page) {
-//        Page<Gestor> gestores = gestorService.getAllGestores(page);
-//        return ResponseEntity.ok(gestores);
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<Gestor> getById(@PathVariable String id) {
-//        Optional<Gestor> gestor = gestorService.getGestorById(id);
-//        return gestor.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-//    }
-//
-////    @PutMapping("/{id}")
-////    public ResponseEntity<Gestor> update(@PathVariable Long id, @RequestBody Gestor gestor) {
-////        Gestor updatedGestor = gestorService.updateGestor(id, gestor);
-////        return updatedGestor != null ?
-////                ResponseEntity.ok(updatedGestor) :
-////                ResponseEntity.notFound().build();
-////    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> delete(@PathVariable String id) {
-//        gestorService.deleteGestor(id);
-//        return ResponseEntity.noContent().build();
-//    }
+    @GetMapping("/create")
+    public String gestorCreate(Model model) {
+        model.addAttribute("gestor", new GestorDTO());
+        return "signupGestor";
+    }
+
+    @PostMapping("/create")
+    public String gestorSave(@ModelAttribute("gestor") GestorDTO gestorDTO) {
+        gestorService.createGestor(gestorDTO);
+        return "gestorManagement";
+    }
+
 }
--- Spring/FutebolAmigo/src/main/java/br/ufpe/cin/futebolamigo/dto/GestorDTO.java
+++ Spring/FutebolAmigo/src/main/java/br/ufpe/cin/futebolamigo/dto/GestorDTO.java
@@ -8,18 +8,7 @@ import lombok.Setter;
 public class GestorDTO {
 
     private String cpf;
-    private String nome;
-    private String email;
-
-    // Opcional: ID do time (simplificado)
-//    private Long timeId;
-
-    // Construtor com todos os campos (opcional)
-//    public GestorDTO(String cpf, String nome, String email, Long timeId) {
-//        this.cpf = cpf;
-//        this.nome = nome;
-//        this.email = email;
-//        this.timeId = timeId;
-//    }
+    private Long idUser;
+//    private Long idTime;
 
 }
