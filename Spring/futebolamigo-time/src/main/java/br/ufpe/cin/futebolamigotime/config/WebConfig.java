ecaba0fcd7210ef413ebf46aab6de533dde5f3d7
PedrovlBarbosa <pvlb@cin.ufpe.br>
2024-03-10 15:20:18 -0300

    .config

---
+++ Spring/futebolamigo-time/src/main/java/br/ufpe/cin/futebolamigotime/config/WebConfig.java
@@ -0,0 +1,25 @@
+package br.ufpe.cin.futebolamigotime.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
+    }
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+
+
+
+
+
+}
