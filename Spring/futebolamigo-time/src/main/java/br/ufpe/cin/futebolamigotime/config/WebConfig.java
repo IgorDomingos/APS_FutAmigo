6c67a6d9d92f77d3418787e3150b2c364b632cf9
PedrovlBarbosa <pvlb@cin.ufpe.br>
2024-03-19 18:55:03 -0300

    update

--- Spring/futebolamigo-time/src/main/java/br/ufpe/cin/futebolamigotime/config/WebConfig.java
+++ Spring/futebolamigo-time/src/main/java/br/ufpe/cin/futebolamigotime/config/WebConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry){
-        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/").setViewName("timeManagement");
 //        registry.addViewController("/login").setViewName("login");
     }
     @Bean
